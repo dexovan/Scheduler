fec2c13f47a7fc3a10a86fe9dd14630376b1be9a
Jonathan Messing <Jonathan.lev.messing@gmail.com>
2015-10-13 16:26:36 -0400

    Beginning styling

--- app.js
+++ app.js
@@ -21,13 +21,6 @@ $("#addEvent").click(function(){
     }
 })
 
- // convertMonthTostring: function(monthNum) {
- //            var monthsStringArray = ["January", "February" ,"March", "April", "May","June", "July", "August", "September", "October", "November", "December"];
- //                var name = monthsStringArray[monthNum]
- //                return name 
- //        }
-
-
 
 var daysOfTheWeek = {
         init: function() {
@@ -45,7 +38,7 @@ var daysOfTheWeek = {
             for(var i = 0; i <= DaysToAdd; i++){
                  var currentDate = new Date();
                  currentDate.setDate(startDate.getDate() + i);
-                 arDates.push(currentDate.getDate() + ' ' + DayAsString[currentDate.getDay()] )
+                 arDates.push(currentDate.getDate() + ' ' +DayAsString[currentDate.getDay()] )
             }
             console.log(arDates)
 
@@ -58,18 +51,6 @@ var daysOfTheWeek = {
 };
 
 
-
-// var d = new Date();
-// var weekday = new Array(7);
-// weekday[0]=  "Sunday";
-// weekday[1] = "Monday";
-// weekday[2] = "Tuesday";
-// weekday[3] = "Wednesday";
-// weekday[4] = "Thursday";
-// weekday[5] = "Friday";
-// weekday[6] = "Saturday";
-
-// var n = weekday[d.getDay()];
 
 
 
