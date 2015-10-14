3b760429fdc4658ea3eb9e80345358f4d46e5a93
Jonathan Messing <Jonathan.lev.messing@gmail.com>
2015-10-14 02:16:24 -0400

    shows current m/y/d on top

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
 $(document).ready(function(){
 
-var getCurrentYearAndMonth = function(){
-    var date = new Date();
+
+var getCurrentYearMonthDay = (function() {
+
+    var months = ["January", "February","March","April","May","June","July","August", "September", "October", "November","December" ]
+     var date = new Date();
     var year = date.getFullYear();
     var monthNum = date.getMonth();
-    convertMonthTostring(monthNum)
-    return year
-}
+    var dayNum = date.getDay();
+   
+     $('#yearMonth').html(months[monthNum] + ' ' + year);
+
+    function ordinal_suffix_of(dayNum) {
+        var j = dayNum % 10,
+            k = dayNum % 100;
+            if (j == 1 && k != 11) {
+                return dayNum + "st";
+            }
+            if (j == 2 && k != 12) {
+                return dayNum + "nd";
+            }
+            if (j == 3 && k != 13) {
+                return dayNum + "rd";
+            }
+               return dayNum + "th";
+            }
+
+     $('#date').html(ordinal_suffix_of(dayNum))
+   
+}());
+
+
+
+
+
 
 var daysOfMonth = function(year, month){
     var numberOfDays = new Date(year, month, 0);
     return numberOfDays.getDate();
 }
 
-$("#addEvent").click(function(){
-    if($('#addToCalander').val() == ''){
-        console.log("Please enter an event")
-    } else {
-      var addedEvent = $('#addToCalander').val()
-    }
-})
+// $("#addEvent").click(function(){
+//     if($('#addToCalander').val() == ''){
+//         console.log("Please enter an event")
+//     } else {
+//       var addedEvent = $('#addToCalander').val()
+//     }
+// })
 
 
 var daysOfTheWeek = {
@@ -40,13 +67,8 @@ var daysOfTheWeek = {
                  currentDate.setDate(startDate.getDate() + i);
                  arDates.push(currentDate.getDate() + ' ' +DayAsString[currentDate.getDay()] )
             }
-            console.log(arDates)
-
-        },
-        Render: function() {
 
         }
-
        
 };
 
